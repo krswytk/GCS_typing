45b875b119d4f0ecfa83d9b657b0b8b33cb3fd7c
CurtanaOriginal <titan7312@gmail.com>
2020-07-28 21:24:48 +0900

    シーン変更

---
+++ GCS_typing/Assets/Script/Dictionary/GetClickedGameObject.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class GetClickedGameObject : MonoBehaviour
+{
+
+    public GameObject clickedGameObject;
+
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            clickedGameObject = null;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit = new RaycastHit();
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                clickedGameObject = hit.collider.gameObject;
+            }
+
+            Debug.Log(clickedGameObject);
+        }
+    }
+}
