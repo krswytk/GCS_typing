8fe35e97eb9a7b6b6e0097ba27c80704dadcc258
katakura_rintaro <rinta2305@gmail.com>
2020-08-01 21:31:30 +0900

    main

--- GCS_typing/Assets/Script/Main/Text_move.cs
+++ GCS_typing/Assets/Script/Main/Text_move.cs
@@ -38,15 +38,15 @@ public class Text_move : MonoBehaviour
 
             if (Lenth[Line] % 2 == 0)
             {
-                add.x = -5.13f - (((Lenth[Line]) / 2) * 0.24f);
-                pos.x = -5.13f - (((Lenth[Line]) / 2) * 0.24f);
-                Debug.Log("Lenth[Line] % 2 == 0");
+                add.x = -5.2f - (((Lenth[Line]) / 2) * 0.23f);
+                pos.x = -5.2f - (((Lenth[Line]) / 2) * 0.23f);
+                //Debug.Log("Lenth[Line] % 2 == 0");
             }
             else
             {
-                add.x = -5.3f - (((Lenth[Line] - 1) / 2) * 0.24f);
-                pos.x = -5.3f - (((Lenth[Line] - 1) / 2) * 0.24f);
-                Debug.Log("Lenth[Line] % 2 == 1");
+                add.x = -5.3f - (((Lenth[Line] - 1) / 2) * 0.23f);
+                pos.x = -5.3f - (((Lenth[Line] - 1) / 2) * 0.23f);
+                //Debug.Log("Lenth[Line] % 2 == 1");
             }
 
             if (ret % 2 == 0)//奇数 次ここ ret=0は一行目　2は3行目
@@ -54,12 +54,12 @@ public class Text_move : MonoBehaviour
                 //add.y = 1.6f + ((ret/2)* 0.13f);
                 //add.y = 1.6f + (((ret / 2) * 0.072f)* (ret / 2));
                 add.y = 1.45f + ((ret/ 2) * 0.27f);
-                Debug.Log("ret % 2 == 0");
+                //Debug.Log("ret % 2 == 0");
             }
             else//偶数　ここは完成
             {
                 add.y = 1.6f + (((ret - 1) / 2) * 0.27f);
-                Debug.Log("ret % 2 == 1");
+                //Debug.Log("ret % 2 == 1");
             }
             myTransform.position = add;
             sw = true;
@@ -68,33 +68,42 @@ public class Text_move : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Space))// 9文字で2.1だけ移動
         {
-            if ((add.x - pos.x) < (Lenth[Line] - 2) * 0.234)
+            if (Line < ret + 1)
             {
-                add.x += 0.233f;
-                myTransform.position = add;
-            }else//奇数と偶数分け
+                if ((add.x - pos.x) < (Lenth[Line] - 2) * 0.234)
+                {
+                    add.x += 0.233f;
+                    myTransform.position = add;
+                }
+                else//奇数と偶数分け
+                {
+                    Line++;
+                    add.y -= 0.27f;//行を下に移動1.95 1.579
+
+                    if (Lenth[Line] % 2 == 0)
+                    {
+                        add.x = -5.2f - (((Lenth[Line]) / 2) * 0.23f);
+                        pos.x = -5.2f - (((Lenth[Line]) / 2) * 0.23f);
+                        //Debug.Log("Lenth[Line] % 2 == 0");
+                    }
+                    else
+                    {
+                        add.x = -5.3f - (((Lenth[Line] - 1) / 2) * 0.23f);
+                        pos.x = -5.3f - (((Lenth[Line] - 1) / 2) * 0.23f);
+                        //Debug.Log("Lenth[Line] % 2 == 1");
+                        //Debug.Log((((Lenth[Line] - 1) / 2) * 0.241f));
+                    }
+
+                    //add.x = -5.25f - (((Lenth[Line] - 1) / 2) * 0.24f);//9文字で2
+                    //pos.x = -5.25f - (((Lenth[Line] - 1) / 2) * 0.24f);
+                    myTransform.position = add;
+                }
+                //Debug.Log("Line:" + Line + "ret:" + ret);//17文字 7.22 7.1425  7文字  6.02
+            }else
             {
-                Line++;
-                add.y -= 0.27f;//行を下に移動1.95 1.579
-
-                if (Lenth[Line] % 2 == 0)
-                {
-                    add.x = -5.13f - (((Lenth[Line]) / 2) * 0.24f);
-                    pos.x = -5.13f - (((Lenth[Line]) / 2) * 0.24f);
-                    Debug.Log("Lenth[Line] % 2 == 0");
-                }
-                else
-                {
-                    add.x = -5.3f - (((Lenth[Line] - 1) / 2) * 0.24f);
-                    pos.x = -5.3f - (((Lenth[Line] - 1) / 2) * 0.24f);
-                    Debug.Log("Lenth[Line] % 2 == 1");
-                }
-
-                //add.x = -5.25f - (((Lenth[Line] - 1) / 2) * 0.24f);//9文字で2
-                //pos.x = -5.25f - (((Lenth[Line] - 1) / 2) * 0.24f);
-                myTransform.position = add;
+                Debug.Log("CLEAR!");
             }
         }
-        Debug.Log(myTransform.position.x);//17文字 7.22  7文字  6.02
+        
     }
 }
