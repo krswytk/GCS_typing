560a53d0bff708ae0b2ef4164b0c0048c6638de9
TakanoMai <shadowcat.game.mai@gmail.com>
2020-08-02 17:36:13 +0900

    スタートからチョイスへの遷移

---
+++ GCS_typing/Assets/Script/Start/titlestart.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class titlestart : MonoBehaviour
+{
+
+    public void OnClickStartButton() //https://dianxnao.com/ボタンクリックでシーン間を遷移%ef%bc%88移動%ef%bc%89する/
+    {
+        SceneManager.LoadScene("Choice");
+    }
+}
