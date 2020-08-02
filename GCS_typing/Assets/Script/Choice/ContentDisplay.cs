74d9ae19ccce618c8c952d08064669ed392449ac
krswytk <41615488+krswytk@users.noreply.github.com>
2020-08-02 22:26:28 +0900

    Revert "Revert "原稿内容表示実装　メニュー画面全行程終了""

---
+++ GCS_typing/Assets/Script/Choice/ContentDisplay.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ContentDisplay : MonoBehaviour
+{
+    private Text text;
+    private FileNumber FN;
+    private Onturn OT;
+    GameObject Manuscript;
+    int num;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = this.GetComponent<Text>();
+        Manuscript = GameObject.Find("TestManuscripS");
+        FN = Manuscript.GetComponent<FileNumber>();
+        OT = Manuscript.GetComponent<Onturn>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        num = OT.GetNum();
+        Debug.Log("num : " + num);
+        text.text = FN.D[num].GetText();
+    }
+}
