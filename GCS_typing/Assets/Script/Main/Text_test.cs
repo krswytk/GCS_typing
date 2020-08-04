b807550b9deb6f5275396d1ac20f01459e1a515e
katakura_rintaro <rinta2305@gmail.com>
2020-08-04 18:51:13 +0900

    メインの表記方法を変更

---
+++ GCS_typing/Assets/Script/Main/Text_test.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Text_test : MonoBehaviour
+{
+    [SerializeField] private Text text;
+    public static int[] LineLenth;
+    public static int ret;
+    bool sw = false;
+    string[] splitted;
+    int Line = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (LoadText.sw == true)//行ごとの文字数をLineLenth[i]に格納
+        {
+            SplitLengh(text.text);
+            LoadText.sw = false;
+        }
+
+        text.text = splitted[Line];
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Line++;
+        }
+    }
+
+
+    void SplitLengh(string str)
+    {
+        string a = str;
+
+        string before = str;
+        string after = str.Replace("\n", "");
+        ret = before.Length - after.Length;
+
+
+        int[] Lenth = new int[ret + 1];
+        LineLenth = new int[ret + 1];
+
+        string[] del = { "\r\n" };
+        splitted = str.Split(del, StringSplitOptions.None);
+
+        //Debug.Log(ret);
+
+        for (int i = 0; i < ret+1; i++)
+        {
+            Debug.Log(splitted[i]);
+        }
+    }
+
+    void GetLengh(string str)
+    {
+        string a = str;
+
+        string before = str;
+        string after = str.Replace("\n", "");
+        ret = before.Length - after.Length;
+
+
+        int[] Lenth = new int[ret + 1];
+        LineLenth = new int[ret + 1];
+
+        //Debug.Log(ret);
+
+        for (int i = 0; i < ret; i++)
+        {
+            if (i != 0)
+            {
+                Lenth[i] = a.IndexOf('\n', Lenth[i - 1] + i + i + 2) - i - i - 1;
+            }
+            else
+            {
+                Lenth[i] = a.IndexOf('\n', 0) - 1;
+            }
+
+            //Debug.Log("Lenth:" + Lenth[i]+"  i:" + i);
+        }
+        Lenth[ret] = before.Length - (ret) - (ret);
+        //Debug.Log("Lenth:" + Lenth[ret] + "  i:" + ret);
+
+        for (int i = 0; i < ret + 1; i++)
+        {
+            if (i != 0)
+            {
+                LineLenth[i] = Lenth[i] - Lenth[i - 1];
+            }
+            else
+            {
+                LineLenth[i] = Lenth[i];
+            }
+        }
+    }
+}
