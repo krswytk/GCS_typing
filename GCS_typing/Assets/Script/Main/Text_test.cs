56b1843e60238dcacc5e345d023bde70c80c12bd
katakura_rintaro <rinta2305@gmail.com>
2020-08-05 22:02:23 +0900

    text main

--- GCS_typing/Assets/Script/Main/Text_test.cs
+++ GCS_typing/Assets/Script/Main/Text_test.cs
@@ -19,6 +19,7 @@ public class Text_test : MonoBehaviour
     public GameObject prefab;
     GameObject[] obj;
     int count = 0;
+    float text_x=0;
 
     // Start is called before the first frame update
     void Start()
@@ -42,7 +43,26 @@ public class Text_test : MonoBehaviour
         for (int i = 0; i < splitted[Line].Length; i++)
         {
             str[i] = arr[i].ToString();
-            obj[i] = Instantiate(prefab, new Vector2(transform.position.x*i, transform.position.y), transform.rotation);
+            if(splitted[Line].Length%2==0)//文字数が遇数なら
+            {
+                Debug.Log("偶数");
+                text_x = transform.position.x + (((-(splitted[Line].Length / 2) + i) + 1.5f)/2);
+                Debug.Log(text_x);
+            }
+            else//文字数が奇数なら
+            {
+                Debug.Log("奇数");
+                if (i== (splitted[Line].Length-1) / 2)
+                {
+                    text_x = transform.position.x;
+                    Debug.Log(transform.position.x);
+                }
+                else
+                {
+                    text_x = transform.position.x + ((-((splitted[Line].Length - 1) / 2) + i)/1.5f);
+                }
+            }
+            obj[i] = Instantiate(prefab, new Vector2(text_x, transform.position.y), transform.rotation);
             obj[i].transform.parent = transform;
             obj[i].transform.localScale = new Vector3(1, 1, 1);//希望する値
             obj[i].GetComponent<Text>().text = str[i];
@@ -76,7 +96,26 @@ public class Text_test : MonoBehaviour
                 for (int i = 0; i < splitted[Line].Length; i++)
                 {
                     str[i] = arr[i].ToString();
-                    obj[i] = Instantiate(prefab, new Vector2(transform.position.x * i, transform.position.y), transform.rotation);
+                    if (splitted[Line].Length % 2 == 0)//文字数が遇数なら
+                    {
+                        Debug.Log("偶数");
+                        text_x = transform.position.x + (((-(splitted[Line].Length / 2) + i) + 1.5f) / 2);
+                        Debug.Log(text_x);
+                    }
+                    else//文字数が奇数なら
+                    {
+                        Debug.Log("奇数");
+                        if (i == (splitted[Line].Length - 1) / 2)
+                        {
+                            text_x = transform.position.x;
+                            Debug.Log(transform.position.x);
+                        }
+                        else
+                        {
+                            text_x = transform.position.x + ((-((splitted[Line].Length - 1) / 2) + i) / 1.5f);
+                        }
+                    }
+                    obj[i] = Instantiate(prefab, new Vector2(text_x, transform.position.y), transform.rotation);
                     obj[i].transform.parent = transform;
                     obj[i].transform.localScale = new Vector3(1, 1, 1);//希望する値
                     obj[i].GetComponent<Text>().text = str[i];
