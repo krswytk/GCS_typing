d40858f663186e24172e8a610213d9ff9662dff0
CurtanaOriginal <titan7312@gmail.com>
2020-08-06 21:45:27 +0900

    衝突修正

---
+++ GCS_typing/Assets/Script/Dictionary/NodePrefub.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NodePrefub : MonoBehaviour
+{
+
+    [SerializeField]
+    RectTransform prefab = null;
+    [SerializeField]
+    private TextAsset textAsset;
+    private string loadText1;
+    private string[] splitText1;
+    private string[] splitText2;
+
+    void Start()
+    {
+        loadText1 = textAsset.text;
+        splitText1 = loadText1.Split(char.Parse("\n"));
+        splitText2 = loadText1.Split(new char[] {' ', '\n'});
+
+        for (int i = 0; i < splitText1.Length - 2; i++)
+        {
+            var item = GameObject.Instantiate(prefab) as RectTransform;
+            item.SetParent(transform, false);
+
+            var text = item.GetComponentInChildren<Text>();
+            var g_object = item.GetComponent<GetClickedText>();
+
+            text.text = splitText2[i * 3];
+            g_object.setNumber(i);
+        }
+    }
+}
