1bea03a0d7982a0e6fc64fa4a1199942ce7701f1
krswytk <krswytk.0228@gmail.com>
2020-08-07 21:36:04 +0900

    チョイスに単語（漢字）と意味をメインに渡すように原稿を渡していた関数を変更

--- GCS_typing/Assets/Script/Main/GetText.cs
+++ GCS_typing/Assets/Script/Main/GetText.cs
@@ -5,15 +5,6 @@ using UnityEngine;
 public class GetText : MonoBehaviour
 {
     public string[] text;
-    // Start is called before the first frame update
-    void Start()
-    {
-       Debug.Log(text);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public string[] word;
+    public string[] meaning;
 }
