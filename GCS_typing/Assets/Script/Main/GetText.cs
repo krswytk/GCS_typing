d6b7a230f8eb6c6b808eb0c7002e772de6ae2f41
krswytk <krswytk.0228@gmail.com>
2020-08-21 17:56:55 +0900

    no message

--- GCS_typing/Assets/Script/Main/GetText.cs
+++ GCS_typing/Assets/Script/Main/GetText.cs
@@ -6,6 +6,7 @@ public class GetText : MonoBehaviour
 {
     public string[] text;　　//各配列に問題文が入っている
     public string[,,] debris; //各配列に出題用単語が入っている 2字要素　0に単語 1にひらがな　2にローマ字
+    //[問題番号0-9  ,  回答0-3  ,  0に単語 1にひらがな　2にローマ字]
     public string[] word;    //各配列に辞書の単語が入っている
     public string[] meaning; //上の要素と同じ番号に単語の意味が入っている
     /*
