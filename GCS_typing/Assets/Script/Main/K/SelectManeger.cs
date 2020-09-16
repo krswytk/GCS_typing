c785b8333604ecbb6f062e0b0cac624636782b59
DESKTOP-31OM9TO\ひざがみ <krswytk.0228@gmail.com>
2020-09-17 08:06:23 +0900

    no message

--- GCS_typing/Assets/Script/Main/K/SelectManeger.cs
+++ GCS_typing/Assets/Script/Main/K/SelectManeger.cs
@@ -11,6 +11,7 @@ public class SelectManeger : MonoBehaviour
     private int Difficulty;//デブリの数＝難易度を格納する
     private GameObject Basket;
     private GameObject[] Box;
+    private GameObject parent;
 
 
 
@@ -22,6 +23,7 @@ public class SelectManeger : MonoBehaviour
         number = AllManeger.GetNumber();//ゲットテキストのゲットテキストスクリプトを取得
         Difficulty = AllManeger.GetDifficulty();//難易度を格納
         Basket = AllManeger.GetBasket();
+        parent = AllManeger.GetBasketBox();
         Box = new GameObject[Difficulty];
         GenerateBox();
     }
@@ -37,30 +39,34 @@ public class SelectManeger : MonoBehaviour
         switch (Difficulty)
         {
             case 2:
-                Box[n] = Instantiate(Basket,new Vector2(610f, 670f), Quaternion.identity);
+                Box[n] = Instantiate(Basket,new Vector2(610f, 670f), Quaternion.identity, parent.transform);
                 n++;
-                Box[n] = Instantiate(Basket, new Vector2(1310f, 670f), Quaternion.identity);
+                Box[n] = Instantiate(Basket, new Vector2(1310f, 670f), Quaternion.identity, parent.transform);
                 n++;
                 break;
             case 3:
-                Box[n] = Instantiate(Basket, new Vector2(510f, 670f), Quaternion.identity);
+                Box[n] = Instantiate(Basket, new Vector2(510f, 670f), Quaternion.identity, parent.transform);
                 n++;
-                Box[n] = Instantiate(Basket, new Vector2(960f, 670f), Quaternion.identity);
+                Box[n] = Instantiate(Basket, new Vector2(960f, 670f), Quaternion.identity, parent.transform);
                 n++;
-                Box[n] = Instantiate(Basket, new Vector2(1410f, 670f), Quaternion.identity);
+                Box[n] = Instantiate(Basket, new Vector2(1410f, 670f), Quaternion.identity, parent.transform);
                 n++;
                 break;
             case 4:
-                Box[n] = Instantiate(Basket, new Vector2(310f, 670f), Quaternion.identity);
+                Box[n] = Instantiate(Basket, new Vector2(310f, 670f), Quaternion.identity, parent.transform);
                 n++;
-                Box[n] = Instantiate(Basket, new Vector2(730f, 670f), Quaternion.identity);
+                Box[n] = Instantiate(Basket, new Vector2(730f, 670f), Quaternion.identity, parent.transform);
                 n++;
-                Box[n] = Instantiate(Basket, new Vector2(1190f, 670f), Quaternion.identity);
+                Box[n] = Instantiate(Basket, new Vector2(1190f, 670f), Quaternion.identity, parent.transform);
                 n++;
-                Box[n] = Instantiate(Basket, new Vector2(1610f, 670f), Quaternion.identity);
+                Box[n] = Instantiate(Basket, new Vector2(1610f, 670f), Quaternion.identity, parent.transform);
                 n++;
                 break;
             default:Debug.LogError("選択肢表示のボックス生成でエラー");break;
         }
     }
-}
+    public GameObject[] GetBox()
+    {
+        return Box;
+    }
+} 
