ff91f0c1dded27a15814e99b3f1318e111b034e8
DESKTOP-31OM9TO\ひざがみ <krswytk.0228@gmail.com>
2020-09-17 20:34:13 +0900

    辞書に単語を送り込み

---
+++ GCS_typing/Assets/Script/Main/K/D/debuggettext.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class debuggettext : MonoBehaviour
+{
+    GetText GetText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetText = GameObject.Find("GetText").GetComponent<GetText>();
+        for (int i = 0; i < GetText.word.Length; i++)
+        {
+            Debug.Log(GetText.word[i]);
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
