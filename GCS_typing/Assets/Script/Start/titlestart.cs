91dc85dfb63b625d8dc35f10f4553b5d42ad354e
DESKTOP-31OM9TO\ひざがみ <krswytk.0228@gmail.com>
2020-10-08 17:27:38 +0900

    ボタンのイメージを新しいものに変更

--- GCS_typing/Assets/Script/Start/titlestart.cs
+++ GCS_typing/Assets/Script/Start/titlestart.cs
@@ -9,6 +9,7 @@ public class titlestart : MonoBehaviour
 
     public void OnClickStartButton() //https://dianxnao.com/ボタンクリックでシーン間を遷移%ef%bc%88移動%ef%bc%89する/
     {
+        Debug.Log("原稿選択へ");
         SceneManager.LoadScene("Choice");
     }
 
