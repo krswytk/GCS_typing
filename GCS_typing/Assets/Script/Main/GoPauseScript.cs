5dd315b7fc9114f0aca1d23f239fd5fb3361009e
osamoya <osamumoyamoyasii@yahoo.co.jp>
2020-10-15 18:55:51 +0900

    とりあえず呼び出せるように。消せない Merge branch 'master2' of https://github.com/krswytk/GCS_typing into osamoya
    # Conflicts:
    #	GCS_typing/Assets/Script/Main/CheckTestScript.cs

---
+++ GCS_typing/Assets/Script/Main/GoPauseScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoPauseScript : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            Debug.Log("1回");
+            SceneManager.LoadScene("Pause", LoadSceneMode.Additive);
+        }
+    }
+}
