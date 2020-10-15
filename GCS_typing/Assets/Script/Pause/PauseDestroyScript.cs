5081a48f0f5dff334fd7160168028a0f468fa8a7
osamoya <osamumoyamoyasii@yahoo.co.jp>
2020-10-15 18:28:42 +0900

    新しいシーンを作り、下準備など

---
+++ GCS_typing/Assets/Script/Pause/PauseDestroyScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseDestroyScript : MonoBehaviour
+{
+    
+    public void PauseDestroyfunc()
+    {
+        Scene scene = SceneManager.GetSceneByName("Pause");//合体しているうちの、こっちだけ
+        SceneManager.UnloadSceneAsync(scene);
+
+    }
+}
