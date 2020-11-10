c1d00ee48db3872b6fcc1f5aa0661018acf03618
TakanoMai <shadowcat.game.mai@gmail.com>
2020-11-10 11:13:01 +0900

    おねがいします

--- GCS_typing/Assets/Script/Main/N[/Timer.cs
+++ GCS_typing/Assets/Script/Main/N[/Timer.cs
@@ -4,23 +4,18 @@ using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
-    public GoPauseScript GoPauseScript;
 
     public static float countTime = 0;
     // Use this for initialization
     void Start()
     {
-        countTime = 0;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!GoPauseScript.paused)
-        {
-            countTime += Time.deltaTime; //スタートしてからの秒数を格納
-        }
-        
+        countTime += Time.deltaTime; //スタートしてからの秒数を格納
         //if(countTime > 99)
         GetComponent<Text>().text =countTime.ToString("F2"); //小数2桁にして表示
     }
